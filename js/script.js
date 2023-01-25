025cbda04d95cddf59e55da5a3b6a7194837f55e
seykoub <69043080+seykoub@users.noreply.github.com>
2023-01-25 01:31:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function alerter(){
+    alert("ce site est en cours de développement mais vous pouvez continuer votre visite et merci de votre compréhension!")
+};
+/*function formulaire(nom, email, message){
+    this.nom = nom;
+    this.email = email;
+    this.message = message;
+
+}
+
+console.log(formulaire);
+
+sendData(){
+    
+};
+*/
